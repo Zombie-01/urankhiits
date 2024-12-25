85f426886c22f547a7d9e411f17412eee245b646
zombie <mm6816557@gmail.com>
2024-12-25 18:37:40 +0800

    asd

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -160,7 +160,7 @@ const DashboardPage = () => {
         <div className="bg-white shadow-md rounded-lg p-6">
           <h3 className="text-xl font-semibold">Static Cost</h3>
           <p className="text-2xl">
-            ${Number(+generatedCount * 0.4).toFixed(2)}
+            ${Number(+generatedCount * 0.5).toFixed(2)}
           </p>
         </div>
       </div>
